c0b6663724fedbbd874d51d393b3dd03026ce68d
Ayesha-678 <59668834+Ayesha-678@users.noreply.github.com>
2021-02-27 12:02:54 -0600

    change

--- BondageClub/Screens/Room/PHCell1/PHCell1.js
+++ BondageClub/Screens/Room/PHCell1/PHCell1.js
@@ -2,11 +2,11 @@
 var PHCell1Background = "Cell";
 
 function PHCell1Run() {
-    DrawCharacter(Player, 250, 0, 1);
+    DrawCharacter(Player, 750, 0, 1);
 
 }
 
 function PHCell1Click() {
-    if (MouseIn(250, 0, 500, 1000)) CharacterSetCurrent(Player);
+    if (MouseIn(750, 0, 500, 1000)) CharacterSetCurrent(Player);
 
 }
