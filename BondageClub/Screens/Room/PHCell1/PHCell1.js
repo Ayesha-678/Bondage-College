b72e0d9f2474b29b9e978a3e9a85ea878fd10a80
Ayesha-678 <59668834+Ayesha-678@users.noreply.github.com>
2021-03-11 15:14:23 -0600

    5

--- BondageClub/Screens/Room/PHCell1/PHCell1.js
+++ BondageClub/Screens/Room/PHCell1/PHCell1.js
@@ -1,12 +1,26 @@
 "use strict";
 var PHCell1Background = "Cell";
+var PHCell1Vent = null;
 
 function PHCell1Run() {
     DrawCharacter(Player, 750, 0, 1);
+    DrawImage("Screens/Room/PHCell1/Vent.png", 50,-600)
+
 
 }
 
 function PHCell1Click() {
     if (MouseIn(750, 0, 500, 1000)) CharacterSetCurrent(Player);
+    if (MouseIn(50, 400, 500, 600)) CharacterSetCurrent(PHCell1Vent);
 
+
+}
+
+function PHCell1Load() {
+    if (!PHCell1Vent) {
+        PHCell1Vent =
+        CharacterLoadNPC("NPC_PHCell1_Vent");
+        PHCell1Vent.FixedImage = "Screens/Room/PHCell1/Vent.png";
+        PHCell1Vent.AllowItem = false;
+    }
 }
